12d4c092104231436edd961818e28d5f7df09a1a
rnkaufman <nina@procore.com>
2016-10-07 06:09:04 +0000

    Elixir support

--- spec/euler_spec.rb
+++ spec/euler_spec.rb
@@ -26,6 +26,7 @@ describe Euler do
 
   it "should have a few languages preregistered" do
     Euler.get_language('coffeescript').should be_truthy
+    Euler.get_language('elixir').should be_truthy
     Euler.get_language('haskell').should be_truthy
     Euler.get_language('java').should be_truthy
     Euler.get_language('javascript').should be_truthy
