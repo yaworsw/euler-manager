abd848dd55253a00b5a71554dfc74677a04ea212
William Yaworsky <will.yaworsky@gmail.com>
2016-10-10 18:15:00 -0400

    Improved examples
    removed haskell
    solved problem 1 in ruby and scala

--- example/1/ruby/1.rb
+++ example/1/ruby/1.rb
@@ -1,5 +1,11 @@
 require_relative '../../lib/euler.rb'
 
-answer = 0
+one_to_one_thousand = (1...1000).to_a
+
+divisible_by_three_or_five = lambda { |num|
+  num % 3 == 0 || num % 5 == 0
+}
+
+answer = one_to_one_thousand.select(&divisible_by_three_or_five).reduce(:+)
 
 puts answer
--- example/1/scala/1.scala
+++ example/1/scala/1.scala
@@ -2,7 +2,7 @@ import euler._
 
 object Main extends App {
 
-  val answer = 0
+  val answer = (1 until 1000).filter((x) => x % 3 == 0 || x % 5 == 0).foldLeft(0)(_+_)
 
   println(answer)
 
