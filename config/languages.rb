5ba7de702b2fbec1935245d993b4273ed872c557
Harman Singh <harman28@gmail.com>
2016-10-15 01:53:32 +0530

    One list to rule them all

---
+++ config/languages.rb
@@ -0,0 +1,16 @@
+class Languages
+  $supportedLanguagesList = [
+    'c',
+    'coffeescript',
+    'elixir',
+    'haskell',
+    'java',
+    'javascript',
+    'julia',
+    'perl',
+    'php',
+    'python',
+    'ruby',
+    'scala'
+  ]
+end
